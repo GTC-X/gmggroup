7748f77448f3415d16f2708146c52686debed533
webteamgtc <144129216+webteamgtc@users.noreply.github.com>
2026-04-13 18:56:20 +0400

    ssaasas

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -37,16 +37,7 @@ export default async function LocaleLayout({ children, params }) {
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
         />
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PSWH9QF');`,
-          }}
-        />
+      
         <style>{`
           body {
             font-family: '${
