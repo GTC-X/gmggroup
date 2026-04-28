241ebb715bfad2b8507e414f3584bcbb10c2c587
adeelnazeer <adeelcomsats070@gmail.com>
2026-04-28 12:38:20 +0500

    Fixed : added GTM tag in layout

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -21,11 +21,22 @@ export default async function LocaleLayout({ children, params }) {
           href="https://fonts.gstatic.com"
           crossOrigin=""
         />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TWL284GN');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
-         <meta
+        <meta
           name="facebook-domain-verification"
           content="3viith4fykysbzqiwlda4x60ctvrzt"
         />
@@ -37,16 +48,25 @@ export default async function LocaleLayout({ children, params }) {
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
         />
-      
+
         <style>{`
           body {
-            font-family: '${
-              isArabicOrKurdish ? "Noto Kufi Arabic" : "Poppins"
-            }', sans-serif;
+            font-family: '${isArabicOrKurdish ? "Noto Kufi Arabic" : "Poppins"
+          }', sans-serif;
           }
         `}</style>
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TWL284GN"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <NextIntlClientProvider>{children}</NextIntlClientProvider>
         <ToastContainer autoClose={3000} />
       </body>
